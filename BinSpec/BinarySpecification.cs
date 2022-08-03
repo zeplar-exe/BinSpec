8d25bd4e5d8a388d7d0bf9746d07902c3f6ea629
Zander Baker <zanderbaker100@gmail.com>
2022-08-03 14:18:59 -0700

    Create basic specification classes
    [File/Class]: Create BinarySpecification
    [File/Class]: Create ByteSpecifier

---
+++ BinSpec/BinarySpecification.cs
@@ -0,0 +1,41 @@
+﻿namespace BinSpec;
+
+public class BinarySpecification
+{
+    private List<ByteSpecifier> Specifiers { get; set; }
+
+    public long SpecifiedLength => Specifiers.Sum(s => s.ByteCount);
+
+    public BinarySpecification()
+    {
+        Specifiers = new List<ByteSpecifier>();
+    }
+
+    public ByteSpecifier AtLocation(long index)
+    {
+        var sum = 0L;
+
+        foreach (var specifier in Specifiers)
+        {
+            if (index >= sum)
+            {
+                return specifier;
+            }
+            
+            sum += specifier.ByteCount - 1;
+        }
+        
+        throw new IndexOutOfRangeException(
+            $"'{nameof(index)}' ({index}) is outside the bounds of this specification; '{SpecifiedLength}' bytes.");
+    }
+
+    public void AddSpecifier(ByteSpecifier specifier)
+    {
+        Specifiers.Add(specifier);
+    }
+
+    public void CopyTo(BinarySpecification specification)
+    {
+        specification.Specifiers.AddRange(Specifiers);
+    }
+}
---
+++ BinSpec/ByteSpecifier.cs
@@ -0,0 +1,21 @@
+﻿namespace BinSpec;
+
+public class ByteSpecifier
+{
+    public string DebugTypeName { get; }
+    public long ByteCount { get; }
+    
+    public ByteSpecifier(long byteCount) : this("UnknownType", byteCount)
+    {
+        
+    }
+    
+    public ByteSpecifier(string debugTypeName, long byteCount)
+    {
+        if (byteCount < 1)
+            throw new ArgumentException("Byte count must be larger than 0.", nameof(byteCount));
+        
+        DebugTypeName = debugTypeName;
+        ByteCount = byteCount;
+    }
+}
