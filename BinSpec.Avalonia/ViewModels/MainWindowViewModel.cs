36934a0e685a6847009b95c56a8d9f64e3c5fd30
Zander Baker <zanderbaker100@gmail.com>
2022-08-03 14:21:49 -0700

    Create MVVM Avalonia project
    [Project]: Create BinSpec.Avalonia project

---
+++ BinSpec.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BinSpec.Avalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ BinSpec.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BinSpec.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
